9e6da9dce122a8687a1a8909bb2f4e6ae7efa7b9
Sens3ii <densteeper@gmail.com>
2021-07-02 00:42:28 +0600

    refactor: style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -25,27 +25,31 @@ export default class App extends Component {
 						<Header />
 					</div>
 					<div className="container">
-						<Switch>
-							<Route
-								exact
-								path="/"
-								render={() => {
-									return <Redirect to="/home" />;
-								}}
-							/>
-							<Route path="/home" exact component={HomePage} />
-							<Route path="/characters/" exact component={CharacterPage} />
-							<Route path="/books/" exact component={BookPage} />
-							<Route path="/houses/" exact component={HousePage} />
-							<Route
-								path="/books/:id"
-								render={({ match, location, history }) => {
-									const { id } = match.params;
-									return <BookItem bookId={id} />;
-								}}
-							/>
-							<Route path="*" exact component={NotFoundPage} />
-						</Switch>
+						<div className="row justify-content-center">
+							<div className="col-md-9 text-center">
+								<Switch>
+									<Route
+										exact
+										path="/"
+										render={() => {
+											return <Redirect to="/home" />;
+										}}
+									/>
+									<Route path="/home" exact component={HomePage} />
+									<Route path="/characters/" exact component={CharacterPage} />
+									<Route path="/books/" exact component={BookPage} />
+									<Route path="/houses/" exact component={HousePage} />
+									<Route
+										path="/books/:id"
+										render={({ match, location, history }) => {
+											const { id } = match.params;
+											return <BookItem bookId={id} />;
+										}}
+									/>
+									<Route path="*" exact component={NotFoundPage} />
+								</Switch>
+							</div>
+						</div>
 					</div>
 				</div>
 			</Router>
