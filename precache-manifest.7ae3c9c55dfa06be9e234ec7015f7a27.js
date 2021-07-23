22f9e3c3f88e23f470abe25ec6715bbce5433626
Sens3ii <densteeper@gmail.com>
2021-07-23 23:40:13 +0600

    Updates

---
+++ precache-manifest.7ae3c9c55dfa06be9e234ec7015f7a27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51302650b8a8bb04aaff",
+    "url": "/React-Game-Of-Thrones-DB/static/css/main.7e426cd8.chunk.css"
+  },
+  {
+    "revision": "51302650b8a8bb04aaff",
+    "url": "/React-Game-Of-Thrones-DB/static/js/main.51302650.chunk.js"
+  },
+  {
+    "revision": "40672a22e7a0986769b9",
+    "url": "/React-Game-Of-Thrones-DB/static/css/1.c7b8df28.chunk.css"
+  },
+  {
+    "revision": "40672a22e7a0986769b9",
+    "url": "/React-Game-Of-Thrones-DB/static/js/1.40672a22.chunk.js"
+  },
+  {
+    "revision": "2695b2cf3dbb5add0a47",
+    "url": "/React-Game-Of-Thrones-DB/static/js/runtime~main.2695b2cf.js"
+  },
+  {
+    "revision": "608376fe3bfb6a405ae80061207ef01d",
+    "url": "/React-Game-Of-Thrones-DB/static/media/got.608376fe.jpg"
+  },
+  {
+    "revision": "b5f7e8418060df718fea253bd2a799b9",
+    "url": "/React-Game-Of-Thrones-DB/index.html"
+  }
+];
